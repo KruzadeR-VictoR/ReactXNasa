0a576bc51f6e551fb9362ae3b28d825d2c626a39
KruzadeR-VictoR <victorbiju31@gmail.com>
2022-09-20 20:18:22 +0530

    changed CSS  @Hero ,added ParticlesJS

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,55 +2,62 @@ import React, { useContext, useEffect, useState } from "react";
 import "./Hero.css";
 import axios from "axios";
 import Navbar from "./Navbar";
-import bgHero from "../assets/bg-hero.jpg";
 import { dataContext } from "../Pages/Pages";
+import Particle from "../Particles/Particle";
+import screenfull from 'screenfull'
+
 
 function Hero() {
   const { Datas, setDatas, fetchData } = useContext(dataContext);
 
-  const [Apod, setApod] = useState({});
+  const [Apod, setApod] = useState();
 
-  const key = import.meta.env.VITE_API_KEY;
-  console.log(key);
+  const key = import.meta.env.VITE_API_KEY;  
 
   useEffect(() => {
     axios
       .get(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
-      .then((result) => {
-        // console.log(result.data);
+      .then((result) => {        
         setApod(result.data);
       });
   }, []);
+
+  const handleImageFullscreen=(e)=>{
+    if (screenfull.isEnabled) {
+      screenfull.toggle(e.target);
+    }
+  }
  
-  console.log(Apod);
+  // console.log(Apod);
 
   return (
     <>
       <Navbar />
-      <div className="hero-wrapper" style={{ background: `url(${bgHero})` }}>
+      <div className="hero-wrapper">
         <div className="hero">
-          {/* <h3 className="heading">Astronomy Picture of the Day</h3> */}
+          <h1 className="heading">Astronomy Picture of the Day</h1>
           <div className="card">
-            <h2 className="heading">Astronomy Picture of the Day</h2>
+            {/* <h2 className="heading">Astronomy Picture of the Day</h2> */}
             <div className="media">
-              {Apod.media_type === "video" ? (
+              {Apod?.media_type === "video" ? (
                 <>
-                  <iframe src={Apod.url} frameborder="0"></iframe>
+                  <iframe src={Apod?.url} frameborder="0"></iframe>
                 </>
               ) : (
                 <>
                   {/* <iframe src={Apod.url} frameborder="0"></iframe> */}
-                  <img src={Apod.url} alt="" />
+                  <img src={Apod?.url} alt="" onClick={(e)=>handleImageFullscreen(e)}/>
                 </>
               )}
             </div>
             <div className="description">
-              <h1 className="title">{Apod.title}</h1>
-              <p className="explanation">{Apod.explanation}</p>
+              <h1 className="title">{Apod?.title}</h1>
+              <p className="explanation">{Apod?.explanation}</p>
             </div>
           </div>
         </div>
       </div>
+    <Particle/>
     </>
   );
 }
---
+++ src/Particles/Particle.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+
+function Particle() {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#28282B",
+          },
+        },
+        fullScreen: {
+          zIndex: "-1",
+        },
+        fpsLimit: 120,        
+        smooth:'true',        
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#00FFFF",
+          },
+          links: {
+            color: "#986868",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+}
+
+export default Particle;
